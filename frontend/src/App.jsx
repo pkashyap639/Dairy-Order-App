957afcf744a65a5277411019c030b5fc87874384
pkashyap639 <pkashyap_639@hotmail.com>
2026-08-27 21:23:13 -0400

    theme color pallete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <div className="bg-chill-600 text-white p-4 rounded-lg">
+        Tailwind is working
+      </div>
     </>
   );
 }
